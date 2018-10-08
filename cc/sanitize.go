b71a1e31410966f7e6c1edb55db31d02785b844d
Ivan Lozano <ivanlozano@google.com>
2018-10-08 09:29:39 -0700

    Apply minimal runtime to the rest of ubsan
    Make sure that the minimal runtime gets applied to the rest
    of the sanitizers we might use from ubsan.
    
    Bug: 80195448
    Test: Build success and checked build flags.
    Change-Id: I31b211b80bb1068c153a9b65454a2605933c9568
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- cc/sanitize.go
+++ cc/sanitize.go
@@ -42,7 +42,8 @@ var (
 	cfiStaticLibsMutex sync.Mutex
 
 	intOverflowCflags   = []string{"-fsanitize-blacklist=build/soong/cc/config/integer_overflow_blacklist.txt"}
-	minimalRuntimeFlags = []string{"-fsanitize-minimal-runtime", "-fno-sanitize-trap=integer", "-fno-sanitize-recover=integer"}
+	minimalRuntimeFlags = []string{"-fsanitize-minimal-runtime", "-fno-sanitize-trap=integer,undefined",
+		"-fno-sanitize-recover=integer,undefined"}
 )
 
 type sanitizerType int
